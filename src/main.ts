f1b7fc61db4006e37d00e5318d2553ad808e2d34
richardotcom <105524809+richardotcom@users.noreply.github.com>
2024-11-12 21:48:44 +0000

    Authenticate on router

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import dotenv from "dotenv"
+import axios from "axios";
+
+const base_url: string = "https://myrouter.io";
 
 function main(): void {
     dotenv.config();
@@ -9,7 +12,18 @@ function main(): void {
         return;
     }
 
-    console.log(password);
+    // Authentication
+    axios.postForm(base_url + "/check.jst", {
+        username: "admin",
+        password
+    })
+    .then(response => {
+        console.log(response.status);
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.error(error);
+    });
 }
 
 main();
