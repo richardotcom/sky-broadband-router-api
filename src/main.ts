4ad9bf4a4783c89a61cab226855fc9a88cc52d8a
richardotcom <105524809+richardotcom@users.noreply.github.com>
2024-11-17 23:56:36 +0000

    Make code safer by adding more validation

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
+import path from "path";
+import fs from "fs";
 import dotenv from "dotenv"
 import axios from "axios";
 import { CookieJar } from 'tough-cookie';
 import { wrapper } from 'axios-cookiejar-support';
 import * as cheerio from "cheerio";
 
-function main(): void {
-    dotenv.config();
+try {
+    const envPath: string = path.resolve(__dirname, "../.env");
+    if (!fs.existsSync(envPath)) {
+        throw new Error("File " + envPath + " does not exist");
+    }
 
+    dotenv.config({ path: envPath });
     const password: string = process.env.PASSWORD || String();
     if (!password) {
-        console.error('Password not set inside .env file or .env file missing');
-        return;
+        throw new Error("Password not set inside .env file");
     }
 
     // Retain cookies between requests
@@ -29,29 +34,32 @@ function main(): void {
     .then(response => {
         // Parse received HTML to assess success
         const $ = cheerio.load(response.data);
-        const ssid_element = $("#wifissid");
-        if (!ssid_element.length)
+
+        const ssidElement = $("#wifissid");
+        if (!ssidElement.length)
         {
             // Extract error message from alert call embedded inside HTML script
             const alert = response.data.match(/(?<=alert\().+(?=\);)/);
             if (alert) {
                 const message = alert[0].matchAll(/\"(.*?)\"/g);
                 if (message) {
-                    console.error([...message].map(match => match[1]).join(""));
-                    return;
+                    throw new Error([...message].map(match => match[1]).join(""));
                 }
             }
             
-            console.error("Unknown error");
-            return;
+            throw new Error("Unknown error");
+        }
+        if (ssidElement.next()) {
+            console.log("SSID:", ssidElement.next().text());
         }
 
-        console.log("SSID:", $("#wifissid").next().text());
-        console.log("WiFi password:", $("#wifipass").next().text());
+        const passwordElement = $("#wifipass");
+        if (passwordElement.length && passwordElement.next())
+        {
+            console.log("WiFi password:", passwordElement.next().text());
+        }
     })
-    .catch(error => {
-        console.error(error);
-    });
 }
-
-main();
+catch (error) {
+    console.error(error);
+}
