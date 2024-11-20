16762f332e80d759852d0dcf1976acd7fe7c3c5b
richardotcom <105524809+richardotcom@users.noreply.github.com>
2024-11-20 22:57:20 +0000

    Convert API code into singleton class

--- examples/src/main.ts
+++ examples/src/main.ts
@@ -1,26 +1,32 @@
 import path from "path";
 import fs from "fs";
 import dotenv from "dotenv"
-import * as router from "../../lib/src/router-api";
+import { Router } from "../../lib/src/router-api";
 
-try {
+function main() {
     const envPath: string = path.resolve(__dirname, "../../.env");
     if (!fs.existsSync(envPath)) {
-        throw new Error("File " + envPath + " does not exist");
+        console.error("File " + envPath + " does not exist");
     }
 
     dotenv.config({ path: envPath });
     const password: string = process.env.PASSWORD || String();
     if (!password) {
-        throw new Error("Password not set inside .env file");
+        console.error("Password not set inside .env file");
     }
 
-    router.Authenticate(password);
+    Router.getInstance(password)
+    .then(router => {
+        console.log("SSID:", router.getSSID());
+        console.log("WiFi password:", router.getWiFiPassword());
+    })
+    .catch(error => {
+        if (error instanceof Error) {
+            console.error(error.message);
+        } else {
+            console.error("An unknown error occurred");
+        }
+    });
 }
-catch (error: unknown) {
-    if (error instanceof Error) {
-        console.error(error.message);
-    } else {
-        console.error("An unknown error occurred");
-    }
-}
+
+main();
--- lib/src/router-api.ts
+++ lib/src/router-api.ts
@@ -1,48 +1,92 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 import { CookieJar } from "tough-cookie";
 import { wrapper } from "axios-cookiejar-support";
 import * as cheerio from "cheerio";
 
-export function Authenticate(password: string): void {
-    // Retain cookies between requests
-    const jar = new CookieJar();
-    const axiosInstance = axios.create({
-        baseURL: "https://myrouter.io",
-        jar
-    });
-    const client = wrapper(axiosInstance);
+export class Router {
+    // Singleton
+    private static instance: Router;
 
-    // Authentication
-    client.postForm<string>("/check.jst", {
-        username: "admin",
-        password
-    })
-    .then(response => {
-        // Parse received HTML to assess success
-        const $ = cheerio.load(response.data);
+    // HTTP client
+    private client: AxiosInstance;
 
-        const ssidElement = $("#wifissid");
-        if (!ssidElement.length)
-        {
-            // Extract error message from alert call embedded inside HTML script
-            const alert = response.data.match(/(?<=alert\().+(?=\);)/);
-            if (alert) {
-                const message = alert[0].matchAll(/\"(.*?)\"/g);
-                if (message) {
-                    throw new Error([...message].map(match => match[1]).join(""));
+    // Router details
+    private ssid: string = String();
+    private password: string = String();
+
+    private constructor() {
+        // Retain authentication cookies between requests
+        const jar = new CookieJar();
+        const axiosInstance = axios.create({
+            baseURL: "https://myrouter.io",
+            jar
+        });
+        this.client = wrapper(axiosInstance);
+    }
+
+    /**
+     * @returns {Router}
+     * An instance of `Router`.
+     * 
+     * @remarks
+     * Authentication may throw errors which need to be handled by the user.
+     */
+    public static async getInstance(password: string): Promise<Router> {
+        if (!Router.instance) {
+            Router.instance = new Router();
+
+            // Authenticate
+            const response = await Router.instance.client.postForm<string>("/check.jst", {
+                username: "admin",
+                password
+            });
+
+            // Parse received HTML to assess success
+            const $ = cheerio.load(response.data);
+
+            const ssidElement = $("#wifissid");
+            if (!ssidElement.length)
+            {
+                // Extract error message from alert call embedded inside HTML script
+                const alert = response.data.match(/(?<=alert\().+(?=\);)/);
+                if (alert) {
+                    const message = alert[0].matchAll(/\"(.*?)\"/g);
+                    if (message) {
+                        throw new Error([...message].map(match => match[1]).join(""));
+                    }
                 }
+                
+                throw new Error("Unknown error");
             }
-            
-            throw new Error("Unknown error");
-        }
-        if (ssidElement.next()) {
-            console.log("SSID:", ssidElement.next().text());
+
+            //Store details
+            if (ssidElement.next()) {
+                Router.instance.ssid = ssidElement.next().text();
+            }
+
+            const passwordElement = $("#wifipass");
+            if (passwordElement.length && passwordElement.next())
+            {
+                Router.instance.password = passwordElement.next().text();
+            }
         }
 
-        const passwordElement = $("#wifipass");
-        if (passwordElement.length && passwordElement.next())
-        {
-            console.log("WiFi password:", passwordElement.next().text());
-        }
-    });
+        return Router.instance;
+    }
+
+    /**
+     * @returns {string}
+     * SSID as a string.
+     */
+    public getSSID(): string {
+        return this.ssid;
+    }
+
+    /**
+     * @returns {string}
+     * Password of the WiFi in plaintext.
+     */
+    public getWiFiPassword(): string {
+        return this.password;
+    }
 }
